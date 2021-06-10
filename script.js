65652828fe0b50461bf3ffa1bd9cf1b8c3554e28
Panevino <77338033+Panevino@users.noreply.github.com>
2021-06-10 08:53:48 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$(".yes-button").click(function() {
+	$('.Part-Two-Yes').show();
+});
+
+$(".no-button").click(function() {
+	$('.Story-Ending-No').show();
+
+});
+
+$(".doubleclick").dblclick(function() {
+	$('.Story-Ending-Yes').show();
+
+});
